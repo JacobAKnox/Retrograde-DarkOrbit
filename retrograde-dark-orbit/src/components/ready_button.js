5079c3ea04c79c0290a62878d90ebb5052234085
JEmersonLawrance <james.lawrance@oit.edu>
2024-02-26 14:00:59 -0800

    added ready/unready toggle, stylized ReadyButton

--- retrograde-dark-orbit/src/components/ready_button.js
+++ retrograde-dark-orbit/src/components/ready_button.js
@@ -1,7 +1,32 @@
+import { useState } from "react"
+
 export default function ReadyButton() {
+  const [buttonText, setButtonText] = useState("Ready");
+
+  function updateReadyState() {
+    // Server update (mocking the server response at the moment)
+    const response = {
+      status: 200
+    };
+
+    // UI update
+    if (response.status == 200) {
+      toggle();
+    }
+  }
+
+  function toggle() {
+    if (buttonText == "Ready") {
+      setButtonText("Unready");
+    }
+    else {
+      setButtonText("Ready");
+    }
+  }
+
   return (
-    <button role="button">
-      Ready
+    <button className="bg-slate-900 text-white m-1 py-2 px-10 rounded-xl hover:bg-slate-800 disabled:bg-slate-950 disabled:text-gray-700" onClick={() => toggle()} role="button">
+      {buttonText}
     </button>
   )
 }
