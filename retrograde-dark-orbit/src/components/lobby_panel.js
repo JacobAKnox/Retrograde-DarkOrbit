5e11cf9de3113df11291adc57ac28a76fb662a84
JEmersonLawrance <james.lawrance@oit.edu>
2024-02-29 17:42:43 -0800

    add UserList component stub & test

--- retrograde-dark-orbit/src/components/lobby_panel.js
+++ retrograde-dark-orbit/src/components/lobby_panel.js
@@ -1,4 +1,5 @@
 import ChatBox from "./chat_box";
+import UserList from "./user_list";
 import ReadyBox from "./ready_box";
 import ReadyButton from "./ready_button";
 import ReadyStatus from "./ready_status";
@@ -8,6 +9,7 @@ export default function LobbyPanel() {
     <div className="border-4 border-blue-400 min-w-[1000px] min-h-[600px] bg-gray-900 m-20 p-4 rounded-xl flex flex-row">
       <ChatBox/>
       <div className="flex flex-col min-w-[400px] justify-end ml-4 p-4 bg-gray-800 rounded-xl">
+        <UserList/>
         <ReadyBox/>
       </div>
     </div>
---
+++ retrograde-dark-orbit/src/components/user_list.js
@@ -0,0 +1,7 @@
+export default function UserList() {
+  return (
+    <div className="flex items-center justify-center bg-gray-700 min-h-[516px] rounded-xl mb-4">
+      list of users in lobby goes here...
+    </div>
+  )
+}
