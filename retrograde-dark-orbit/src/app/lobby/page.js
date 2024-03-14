7a06653d00611d633b4ac4a369c71ccb5dc38dc9
Jacob Knox <jacob.knox@oit.edu>
2024-03-14 15:43:51 -0700

    #18 removed temp start button

--- retrograde-dark-orbit/src/app/lobby/page.js
+++ retrograde-dark-orbit/src/app/lobby/page.js
@@ -1,7 +1,5 @@
 import ExitLobbyButton from "@/components/exit_lobby_button";
-import ChatBox from "@/components/chat_box";
 import LobbyPanel from "@/components/lobby_panel";
-import GameStartButton from "@/components/temp_start_button";
 import DisplayLobbyCode from "@/components/display_lobby_code";
 import DisplayTimer from "@/components/display_timer";
 export default function Page() {
@@ -9,7 +7,6 @@ export default function Page() {
         <main className="flex justify-center items-center flex-col flex-wrap min-w-full">
             <div>
                 <DisplayTimer/>
-                <GameStartButton/>
             </div>
             <div className="flex flex-row w-1/2 md:justify-start items-center justify-center">
                 <ExitLobbyButton/>
