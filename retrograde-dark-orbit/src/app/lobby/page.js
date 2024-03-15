cb3c22329737ad51556aea8dc1f761fd3cb4c3c3
rayyanansari03 <91793024+rayyanansari03@users.noreply.github.com>
2024-03-14 19:21:22 -0700

    #3

--- retrograde-dark-orbit/src/app/lobby/page.js
+++ retrograde-dark-orbit/src/app/lobby/page.js
@@ -2,7 +2,6 @@
 import ExitLobbyButton from "@/components/exit_lobby_button";
 import ChatBox from "@/components/chat_box";
 import LobbyPanel from "@/components/lobby_panel";
-import GameStartButton from "@/components/temp_start_button";
 import DisplayLobbyCode from "@/components/display_lobby_code";
 import DisplayTimer from "@/components/display_timer";
 import { useSearchParams } from 'next/navigation'
@@ -18,7 +17,6 @@ export default function Page() {
                 <DisplayTimer/>
             </div>
             <LobbyPanel/>
-            <GameStartButton/>
         </main>
     );
 }
